d1ecf1435758d45b4be9467d44fe2019f2bb3f5f
Schlenkerb <bradlyschlenker@gmail.com>
2014-08-13 13:39:28 -0700

    Added blank settings page.

--- backend/settings.php
+++ backend/settings.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_USER', 'root');
-define('DB_PASS', 'pickels');
-define('DB_NAME', 'checkinapp');
-$mysql_access = mysql_connect('vergil.u.washington.edu:3555', DB_USER, DB_PASS, true) or die ('Unable to connect to the Database');
+define('DB_USER', 'your_username');
+define('DB_PASS', 'your_password');
+define('DB_NAME', 'somedatabase');
+$mysql_access = mysql_connect('a_host', DB_USER, DB_PASS, true) or die ('Unable to connect to the Database');
 mysql_select_db(DB_NAME, $mysql_access) or die ('Error: unable to select database:' . mysql_error());
 mysql_query('SET CHARACTER SET utf8');
 
