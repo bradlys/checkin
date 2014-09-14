c449dca284348ac247944aab592baa2ff3da31a6
Schlenkerb <bradlyschlenker@gmail.com>
2014-09-13 17:20:16 -0700

    Renaming

--- backend/html.php
+++ backend/html.php
@@ -71,11 +71,11 @@ function printFooter() {
 
 ?>
     <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/underscore-min.js"></script>
     <script type="text/javascript" src="js/moment-with-locales.min.js"></script>
     <script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
+    <script type="text/javascript" src="js/checkinapp.js"></script>
   </body>
 </html>
 <?php
